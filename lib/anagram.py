9881782fec141d694ddd48e2b6c8435f5b260351
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-07-04 17:00:41 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def is_anagram(self, word):
+        return sorted(self.word) == sorted(word.lower())
+    
+    def match(self, word_list):
+        return [word for word in word_list if self.is_anagram(word)]
+    
